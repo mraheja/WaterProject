5c7ca6e9610a3b246f17f67bcea9cbe04cb4fba7
Mehul Raheja <rahejamehul@gmail.com>
2019-03-23 14:24:17 -0700

    Add files via upload

--- convert.py
+++ convert.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def convert(adress):
-	URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + adress + '&key=AIzaSyBmQGSSO5AYhoCSr9eBc5PNiiY8Cp6mNbQ&callback=initMap'
+	URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + adress + '&key=INSERT_KEY&callback=initMap'
 	r = requests.get(URL).json()
 	for e in r['results']:
 		return (e['geometry']['location'])
